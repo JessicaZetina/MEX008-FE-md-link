42df722292a1962007469a0ecc519df025144478
Laboratoria008 <jessicazetina37@gmail.com>
2019-09-09 15:23:04 -0500

    subiendo proyecto

--- links.js
+++ links.js
@@ -3,14 +3,11 @@ const Marked = require('marked');
 // Función para extraer los links usando marked
 
 
-function markdownLinkExtractor(markdown) {
+function (markdown) {
   const links = [];
 
   const renderer = new Marked.Renderer();
 
-  Marked.InlineLexer.rules.normal.link = linkWithImageSizeSupport;
-  Marked.InlineLexer.rules.gfm.link = linkWithImageSizeSupport;
-  Marked.InlineLexer.rules.breaks.link = linkWithImageSizeSupport;
 
   renderer.link = function(href, title, text) {
     links.push({
